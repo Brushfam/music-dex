510c8107b4f9ef11c21742c5c2686d791fc00e0e
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-03-12 21:26:17 +0200

    updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import s from "./Footer.module.scss";
 import Image from "next/image";
-import {SocialMedia} from "@/components/Footer/SocialMedia";
-import {createSharedPathnamesNavigation} from "next-intl/navigation";
-import {useTranslations} from "next-intl";
+import { SocialMedia } from "@/components/Footer/SocialMedia";
+import { createSharedPathnamesNavigation } from "next-intl/navigation";
+import { useTranslations } from "next-intl";
 
 export function Footer() {
   const { Link: LocalLink } = createSharedPathnamesNavigation({
@@ -19,21 +19,47 @@ export function Footer() {
           width={200}
           height={43}
         />
-        <p className={s.footerMainText}>
-          {t("description")}
-        </p>
+        <p className={s.footerMainText}>{t("description")}</p>
       </div>
       <div className={s.linksBlock}>
-        <SocialMedia/>
+        <SocialMedia />
         <div className={s.linksColumn}>
-          <p style={{ marginBottom: 6 }}>{t("email")}</p>
-          <a href={"mailto:markian@musicdex.co"} target="_blank">markian@musicdex.co</a>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              marginBottom: 6,
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src={"icons/email-footer.svg"}
+              alt={"email"}
+              width={18}
+              height={18}
+              style={{ marginRight: 6 }}
+            />
+            <p>{t("email")}</p>
+          </div>
+          <a
+            href={"mailto:markian@musicdex.co"}
+            target="_blank"
+            style={{ textDecoration: "underline" }}
+          >
+            markian@musicdex.co
+          </a>
         </div>
         <div className={s.linksColumn}>
-          <p style={{marginBottom: 6}}>{t("other")}</p>
-          <LocalLink href={"/documents/privacy-policy"}>{t("privacy_policy")}</LocalLink>
-          <LocalLink href={"/documents/terms-and-conditions"}>{t("terms")}</LocalLink>
-          <LocalLink href={"/documents/public-offer"}>{t("public_offer")}</LocalLink>
+          <p style={{ marginBottom: 6 }}>{t("other")}</p>
+          <LocalLink href={"/documents/privacy-policy"}>
+            {t("privacy_policy")}
+          </LocalLink>
+          <LocalLink href={"/documents/terms-and-conditions"}>
+            {t("terms")}
+          </LocalLink>
+          <LocalLink href={"/documents/public-offer"}>
+            {t("public_offer")}
+          </LocalLink>
         </div>
       </div>
     </div>
