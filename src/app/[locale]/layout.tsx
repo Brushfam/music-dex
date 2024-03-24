c3e46e3e99146e4e905f001464bf70b073b0a282
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-03-24 19:22:51 +0200

    GA script

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.scss";
 import { NextIntlClientProvider, useMessages } from "next-intl";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,21 @@ export default function RootLayout({
   const messages = useMessages();
   return (
     <html lang={locale}>
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-E7NY2W59JZ"
+        />
+
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DRRFYE53MC');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
           {children}
