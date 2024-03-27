ed9a53bbf938312af36095feaa15e94686227f19
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-03-27 08:07:25 +0200

    updated adminList

--- src/data/adminList.js
+++ src/data/adminList.js
@@ -1,7 +1,7 @@
 export const adminList = [
   {
     address: "0x992cE710C1a044cd0e68781dA2c917139C543A43",
-    track: "My Kyiv",
+    track: "Dealer",
     tokenAddress: "0x7a7eAF21e22392b05c4698fC53611D4175b65572",
   },
 ];
