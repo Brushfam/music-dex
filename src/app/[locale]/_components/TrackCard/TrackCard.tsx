1791b769a92983186a57c06e0e52f31a2bd4f7a1
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-03-30 00:41:35 +0200

    updated TrackCard

--- src/app/[locale]/_components/TrackCard/TrackCard.tsx
+++ src/app/[locale]/_components/TrackCard/TrackCard.tsx
@@ -1,11 +1,16 @@
 import s from "./TrackCard.module.scss";
 
-function TrackCardComponent(props: { image: string; title: string }) {
+function TrackCardComponent(props: {
+  image: string;
+  title: string;
+  author: string;
+}) {
   return (
     <div className={s.trackCardWrapper}>
       <div className={s.trackCard}>
         <img src={props.image} alt={props.title} style={{ borderRadius: 4 }} />
         <p className={s.title}>{props.title}</p>
+        <p className={s.author} style={{height: 8}}>{props.author}</p>
       </div>
     </div>
   );
@@ -16,6 +21,7 @@ export function TrackCard1() {
     <TrackCardComponent
       image={"/albums/homepage1.jpg"}
       title={"Coming soon!"}
+      author={""}
     />
   );
 }
@@ -25,6 +31,7 @@ export function TrackCard2() {
     <TrackCardComponent
       image={"/albums/homepage2.jpg"}
       title={"Coming soon!"}
+      author={""}
     />
   );
 }
@@ -34,10 +41,17 @@ export function TrackCard3() {
     <TrackCardComponent
       image={"/albums/homepage3.jpg"}
       title={"Coming soon!"}
+      author={""}
     />
   );
 }
 
 export function TrackCard4() {
-  return <TrackCardComponent image={"/albums/dealer.jpg"} title={"Dealer"} />;
+  return (
+    <TrackCardComponent
+      image={"/albums/dealer.jpg"}
+      title={"Dealer"}
+      author={"Tony Tonite"}
+    />
+  );
 }
