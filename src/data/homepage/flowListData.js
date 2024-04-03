493fed1f38b559c23e28c1cbe3fea735111e59fb
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-04-03 10:05:47 +0300

    small content update

--- src/data/homepage/flowListData.js
+++ src/data/homepage/flowListData.js
@@ -6,7 +6,7 @@ export const flowListDataEN = [
         text: "Investors purchase tokens, becoming stakeholders in the track's royalties"
     },
     {
-        text: "Royalties generated from track streams are distributed to token holders automatically"
+        text: "Royalties are received for listening to tracks on streaming platforms and are distributed among token holders automatically according to the number of tokens they have"
     },
     {
         text: "Investors can track their investments and earnings through our user-friendly interface"
@@ -24,7 +24,7 @@ export const flowListDataUK = [
         text: "Інвестори купують токени, стаючи стейкхолдерами роялті треків"
     },
     {
-        text: "Роялті генеруються зі стрімінгу треків і розподіляються між токенхолдерами автоматично"
+        text: "Роялті надходять за прослуховування треків на стрімінгових платформах і розподіляються між токенхолдерами автоматично відносно кількості токенів які вони мають"
     },
     {
         text: "Інвестори можуть слідкувати за своїми інвестиціями і заробляти через зрозумілий інтерфейс"
