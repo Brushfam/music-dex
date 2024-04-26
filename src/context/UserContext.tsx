20050dd25e5b553443c667c921ebae00d4d7d090
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2024-04-26 21:34:39 +0300

    ArtistModal

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -9,8 +9,10 @@ type UserContentInterface = {
   hasAgreement: string;
   trackOwner: string;
   latestPurchase: string;
+  artistFormStep: string;
   setLatestPurchase: (value: string) => void;
   setHasAgreement: (value: string) => void;
+  setArtistFormStep: (value: string) => void;
   login: (user: string, trackOwnerData: string, wallet: string) => void;
   logout: () => void;
 };
@@ -21,8 +23,10 @@ const UserContext = createContext<UserContentInterface>({
   hasAgreement: "",
   trackOwner: "",
   latestPurchase: "",
+  artistFormStep: "",
   setLatestPurchase(value: string): void {},
   setHasAgreement(value: string): void {},
+  setArtistFormStep(value: string): void {},
   login: () => {},
   logout: () => {},
 });
@@ -32,7 +36,9 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const [wallet, setWallet] = useLocalStorage("user-wallet");
   const [hasAgreement, setHasAgreement] = useLocalStorage("user-agreement");
   const [trackOwner, setTrackOwner] = useLocalStorage("user-track-owner");
-  const [latestPurchase, setLatestPurchase] = useLocalStorage("user-purchase")
+  const [latestPurchase, setLatestPurchase] = useLocalStorage("user-purchase");
+  const [artistFormStep, setArtistFormStep] =
+    useLocalStorage("user-artist-modal");
 
   const login = (
     user: string,
@@ -47,9 +53,8 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => {
     setCurrentUser("");
     setWallet("");
-    // setHasAgreement("");
     setTrackOwner("");
-    setLatestPurchase("")
+    setLatestPurchase("");
   };
 
   return (
@@ -60,8 +65,10 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
         hasAgreement,
         trackOwner,
         latestPurchase,
+        artistFormStep,
         setLatestPurchase,
         setHasAgreement,
+        setArtistFormStep,
         login,
         logout,
       }}
